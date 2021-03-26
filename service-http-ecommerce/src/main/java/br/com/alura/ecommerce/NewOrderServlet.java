ffda39b8a62d37727f9d82272a3283af85544f36
Murilo Prado <kruzprado@hotmail.com>
2021-03-26 15:38:42 -0300

    Httpserver created

---
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -0,0 +1,45 @@
+package br.com.alura.ecommerce;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class NewOrderServlet extends HttpServlet {
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+            var email = req.getParameter("email");
+            var amount = new BigDecimal(req.getParameter("amount"));
+
+            var orderId = UUID.randomUUID().toString();
+
+            var order = new Order(orderId, amount, email);
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+
+            var emailCode = "Thank you for your order! We are processing your order!";
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+
+            var messageReturn = "New order sent successfully.";
+            System.out.println(messageReturn);
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println(messageReturn);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
